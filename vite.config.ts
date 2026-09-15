28f97775e82dea0043feb724985aa890d5e88756
beaco5 <oli.beaco1@gmail.com>
2026-09-15 19:50:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/protraindarlington/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
